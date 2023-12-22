e53bf0e707cb11f50edbd53a24362ee169c96d18
iram <iram@iram-Inspiron-3470>
2023-12-22 11:33:31 +0530

    First commit

---
+++ src/test/java/Adminlogin/portal/Logintoadmin.java
@@ -0,0 +1,19 @@
+package Adminlogin.portal;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class Logintoadmin 
+{
+  @Test
+    public void shouldAnswerWithTrue()
+    {
+        System.out.println("Enter the Username");
+        System.out.println("Enter the Password");
+        System.out.println("Click on login button");
+    }
+}
